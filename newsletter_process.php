54b73ef7bd0fb5579c89743b26f400b4cf44de73
Lucas Delobelle <lucas@ld-web.net>
2019-10-22 16:58:25 +0200

    Début d'utilisation de la BDD (config pas encore refactorisée)

--- newsletter_process.php
+++ newsletter_process.php
@@ -4,23 +4,50 @@ session_start();
 
 use App\Utils;
 
-// TODO: insérer l'email en bdd
-echo $_POST['email'];
+try {
+    if (!file_exists('config/db.ini') || !($dbConfig = parse_ini_file('config/db.ini'))) {
+        throw new Exception("Une erreur est survenue lors du chargement du fichier de configuration");
+    }
+
+    $dsn = 'mysql:host=' . $dbConfig['host'] .
+            ';dbname=' . $dbConfig['dbname'] .
+            ';charset=' . $dbConfig['charset'];
+
+    $pdo = new PDO($dsn, $dbConfig['user'], $dbConfig['password']);
+} catch (Exception $ex) {
+    // TODO: générer nouvelle notification d'erreur avant de rediriger vers la page d'accueil
+    Utils::redirect('index.php');
+}
+
+if (empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    // TODO: générer une notification d'erreur sur le format de l'adresse email
+    Utils::redirect('index.php');
+}
+
+$stmt = $pdo->prepare("INSERT INTO newsletter (email) VALUES (:email)");
+
+$res = $stmt->execute([
+    'email' => $_POST['email']
+]);
+
+var_dump($res);
+
+// TODO: générer notification selon réussite ou échec de la requête
 
 // Enregistrer un message dans la session
 // TODO: Vérifier que la clé notifications est bien vide
 // Sinon, on va écraser toutes les notifications précédentes
 // Donc globalement, on voudra ajouter une notification au tableau
 // Mais vérifier qu'il est défini, et s'il ne l'est pas, le créer
-$_SESSION['notifications'] = [
-    'success' => [
-        'Merci, votre email a bien été enregistré',
-        'T bo'
-    ],
-    'info' => [
-        'RGPD : Votre email ne sera pas divulgué pour de la publicité'
-    ]
-];
+// $_SESSION['notifications'] = [
+//     'success' => [
+//         'Merci, votre email a bien été enregistré',
+//         'T bo'
+//     ],
+//     'info' => [
+//         'RGPD : Votre email ne sera pas divulgué pour de la publicité'
+//     ]
+// ];
 
 // rediriger vers la page d'accueil
-Utils::redirect('index.php');
+//Utils::redirect('index.php');
